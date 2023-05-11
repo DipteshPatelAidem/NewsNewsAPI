d0afb7647c3952a6b61f29262b1fa35e73356ab8
DipteshPatelAidem <dipteshp@aidem.in>
2023-05-11 16:31:58 +0530

    Dynamic log initiated

---
+++ Models/UCNDigitalPreview.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FullStack.API.Models
+{
+    public class UCNDigitalPreview
+    {
+        public string UCNdigitalCode { get; set; }
+        public string[] BrandCode { get; set; }
+     //   public int[] BrandCodes { get; set; }
+    }
+}
