ab381087ca1163b3e015cd80377052ae2342f93a
DipteshPatelAidem <dipteshp@aidem.in>
2023-05-14 15:32:41 +0530

    Given UCN code logic achived. tested with 'VAGLHVASG20ENG00Z5NONSKIP'

--- Controllers/UCNDigitalController.cs
+++ Controllers/UCNDigitalController.cs
@@ -52,29 +52,124 @@ namespace FullStack.API.Controllers
         [Route("GetUCNDigital")]
         public List<UCNDigitalPreview> GetUCNCode(dynamic ucnpreview)
         {
-            string ucnMasterConfig = "[{'key':'brandCode','limit':'5','prifix':'abc','index':'1'}]";
+            string ucnMasterConfig = "[{'key':'brandCode','limit':'5','prefix':'','index':'4'},{'key':'caption','limit':'4','prefix':'-','index':'2'},{'key':'duration','limit':'0','prefix':'-','index':'3'},{'key':'language','limit':'0','prefix':'-','index':'1'},{'key':'destination','limit':'0','prefix':'-','index':'5'},{'key':'format','limit':'0','prefix':'-','index':'6'},{'key':'ratio','limit':'0','prefix':'-','index':'7'}]";
 
             JavaScriptSerializer serializer = new JavaScriptSerializer();
             dynamic masterConfig = serializer.Deserialize<dynamic>(ucnMasterConfig);
+           // masterConfig =Array.Sort(masterConfig,4,1);
+            int MaxLimt = 6;
+            string BrndName = "";
+            string prfix = "";
+//            string[] brandnames;
+            string ucnCode="";
+            List<UCNDigitalPreview> ucnpreviewList = new List<UCNDigitalPreview>();
+            int currentCounter = 0;
+            int processCounter = 0;
+            string CurrentlyProcessFor = "";
 
+
+            // get master config
             foreach (dynamic formula in masterConfig)
             {
-                dynamic key = formula["key"];
-                if (ucnpreview[key])
+                processCounter = 0;
+                currentCounter++;
+                // dynamic key = formula["key"];
+                foreach (dynamic eachAry in formula)
                 {
-                   // dynamic key = formula["key"];
-                    dynamic limit = formula["limit"];
-                    dynamic prefix = formula["prefix"];
+                    if (eachAry.Key == "limit")
+                    {
+                        MaxLimt = Int32.Parse(eachAry.Value);
+                    }
+                    if ((eachAry.Key == "prefix") )
+                    {
+                        prfix = eachAry.Value;
+                    }
+                    if ((eachAry.Key== "index"))
+                    {
+                        processCounter = currentCounter;
+                    }
+                    if ((eachAry.Key == "key") )
+                    {
+                        CurrentlyProcessFor = eachAry.Value;
+                    }
+
+                } // foreach (dynamic eachAry in formula)
+
+                if (CurrentlyProcessFor == "brandCode")
+                {
+                    BrndName = ucnpreview.brandname;
+                    ucnCode = ucnCode + GenerateBrandCode((string)ucnpreview.brandname, MaxLimt);
+                    CurrentlyProcessFor = "";
                 }
+                if (CurrentlyProcessFor == "language")
+                {
+                    string lng = ucnpreview.language;
+                    List<UCNDigitalLanguage> ucnlaguagelist = GetUCNLanguage();
+                    UCNDigitalLanguage selectedlng = ucnlaguagelist.SingleOrDefault(x => x.Language.ToUpper() == lng.ToUpper());
+                    string lngCd = selectedlng.LanguageCode.ToString();
+                    ucnCode = ucnCode + lngCd ; // todo Lannguage code from master
+                    CurrentlyProcessFor = "";
+                }
+                if (CurrentlyProcessFor == "caption")
+                {
+                    string caption = (string)ucnpreview.caption;
+                    ucnCode = ucnCode + GenerateCaptionCode((string)caption, MaxLimt);
+                    CurrentlyProcessFor = "";
+                }
+                if (CurrentlyProcessFor == "duration")
+                {
+                    decimal drn = ucnpreview.duration;
+                    string strdrn = ( drn ).ToString();
+                    ucnCode = ucnCode + (strdrn);
+                    // ucnpreview.duration = Decimal.Parse("99.99"); // todo remove
+                    CurrentlyProcessFor = "";
+                }
+                if (CurrentlyProcessFor == "destination")
+                {
+                    ucnCode = ucnCode + GeneratePlatformCode((string)ucnpreview.platform);
+                    CurrentlyProcessFor = "";
+                }
+                if (CurrentlyProcessFor == "format")
+                {
+                    ucnCode = ucnCode + GenerateFormatCode((string)ucnpreview.format); ;
+                    CurrentlyProcessFor = "";
+                }
+                if (CurrentlyProcessFor == "ratio")
+                {
+                    ucnCode = ucnCode + "";
+                    CurrentlyProcessFor = "";
+                }
+                //if ( formula[0][0]="key")
+                //{
+                //   // dynamic key = formula["key"];
+                //    dynamic limit = formula["limit"];
+                //    dynamic prefix = formula["prefix"];
+                //}
 
-            }
+            } //foreach (dynamic formula in masterConfig)
 
-            List<UCNDigitalPreview> ucnpreviewList = new List<UCNDigitalPreview>();
-            int MaxLimt = 5;
-            string BrndName = ucnpreview.BrandCode[0];
 
+            //ucnpreview.
+
+            var ucnPreviewResponse = new UCNDigitalPreview();
+            ucnPreviewResponse.brandname = ucnpreview.brandname;
+            ucnPreviewResponse.caption = ucnpreview.caption;
+            ucnPreviewResponse.duration = ucnpreview.duration;
+            ucnPreviewResponse.language = ucnpreview.language;
+            ucnPreviewResponse.platform = ucnpreview.platform;
+            ucnPreviewResponse.format = ucnpreview.format;
+
+            ucnPreviewResponse.UCNdigitalCode = ucnCode.ToUpper();
+            ucnpreviewList.Add(ucnPreviewResponse);
+            return ucnpreviewList;
+        }
+
+
+
+        public string GenerateBrandCode(string BrndName, int MaxLimt)
+        {
             string BCode = "";
-            BrndName = BrndName.Trim();
+            BrndName = BrndName.Trim(); // todo handle null here
             string[] aryWords = BrndName.Split(' ');
             int numOfWords = aryWords.Length;
             // BrndName = BrndName.Replace("  "," ");
@@ -113,28 +208,39 @@ namespace FullStack.API.Controllers
                 }
             }
 
-            ucnpreview.UCNdigitalCode = BCode.ToUpper();
-            ucnpreviewList.Add(ucnpreview);
-            return ucnpreviewList;
+            return BCode;
         }
-
-        public static string RemoveSpecialCharacters(string str)
+        public string GenerateCaptionCode(string caption, int MaxLimt)
         {
+           // string cption ;
             StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < str.Length; i++)
+            foreach (char c in caption)
             {
-                if ((str[i] >= '0' && str[i] <= '9')
-                    || (str[i] >= 'A' && str[i] <= 'z'
-                        || (str[i] == '.' || str[i] == '_')))
+                if ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || c == '.' || c == '_')
                 {
-                    sb.Append(str[i]);
+                    sb.Append(c);
                 }
             }
-
-            return sb.ToString();
+            return sb.ToString().Substring(0, MaxLimt);
         }
+        public string GeneratePlatformCode(string platform)
+        {
 
-
+            string platfrm = platform;
+            List<UCNDigitalPlatform> ucnPlatformlist = GetUCNPlatform();
+            UCNDigitalPlatform selectedPlatfrm = ucnPlatformlist.SingleOrDefault(x => x.Platform.ToUpper() == platfrm.ToUpper());
+            string pltfrmCd = selectedPlatfrm.PlatformCode.ToString();
+            pltfrmCd=pltfrmCd.PadLeft(4,'0');
+            return pltfrmCd;
+        }
+        public string GenerateFormatCode(string format)
+        {
+            string frmt = format;
+            List<UCNDigitalFormat> ucnformatlist = GetUCNFormat();
+            UCNDigitalFormat selectedFormat = ucnformatlist.SingleOrDefault(x => x.Format.ToUpper() == frmt.ToUpper());
+            string formatCd = selectedFormat.FormatCode.ToString();
+            return formatCd;
+        }
 
 
         [Route("GetLanguage")]
--- Models/UCNDigitalPreview.cs
+++ Models/UCNDigitalPreview.cs
@@ -6,9 +6,15 @@ using System.Web;
 namespace FullStack.API.Models
 {
     public class UCNDigitalPreview
-    {
+    
+        { 
         public string UCNdigitalCode { get; set; }
-        public string[] BrandCode { get; set; }
-     //   public int[] BrandCodes { get; set; }
+        public string brandname { get; set; }
+        public string caption { get; set; }
+        public decimal duration { get; set; }
+        public string language { get; set; }
+        public string platform { get; set; }
+        public string format { get; set; }
+        public string ratio { get; set; }         
     }
 }
