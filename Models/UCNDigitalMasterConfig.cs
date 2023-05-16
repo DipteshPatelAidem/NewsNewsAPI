224e5c3f92f916de31b4f9fda0a543ac7b4ab167
DipteshPatelAidem <dipteshp@aidem.in>
2023-05-16 09:39:42 +0530

    masterconfig dynamic

---
+++ Models/UCNDigitalMasterConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FullStack.API.Models
+{
+    public class UCNDigitalMasterConfig
+    {
+       // public int MasterConfigID { get; set; }
+        public string key { get; set; }
+        public string limit { get; set; }
+        public string prefix { get; set; }
+        public int index { get; set; }
+        
+    }
+}
