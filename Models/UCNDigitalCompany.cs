b1ac63e24eee6a359af428f9e16843c3b7b08484
DipteshPatelAidem <dipteshp@aidem.in>
2023-05-29 17:19:21 +0530

    Company/Advertiser model created

---
+++ Models/UCNDigitalCompany.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FullStack.API.Models
+{
+    public class UCNDigitalCompany
+    {
+
+        public string CompanyCode { get; set; }
+        public int CompanyID { get; set; }
+        public string Company { get; set; }
+
+    }
+}
