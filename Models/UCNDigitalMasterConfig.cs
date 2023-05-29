d819723163670f91b0aec8ab4cbe370707bdbbd4
DipteshPatelAidem <dipteshp@aidem.in>
2023-05-29 17:20:51 +0530

    masterconfigID reIntroduced with nullable

--- Models/UCNDigitalMasterConfig.cs
+++ Models/UCNDigitalMasterConfig.cs
@@ -7,11 +7,13 @@ namespace FullStack.API.Models
 {
     public class UCNDigitalMasterConfig
     {
-       // public int MasterConfigID { get; set; }
+        public int? MasterConfigID { get; set; }
         public string key { get; set; }
         public string limit { get; set; }
         public string prefix { get; set; }
         public int index { get; set; }
-        
+        public int CompanyID {get; set; }
+        public int EnteredBy { get; set; }
+
     }
 }
