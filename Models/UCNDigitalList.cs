22cfa1436855332178102e2c3477840094ac3ad8
DipteshPatelAidem <dipteshp@aidem.in>
2023-05-29 17:19:48 +0530

    UCNdigital listing model created

---
+++ Models/UCNDigitalList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FullStack.API.Models
+{
+    public class UCNDigitalList
+    {
+        public string UCNdigitalCode { get; set; }
+        public string brandname { get; set; }
+        public string caption { get; set; }
+        public decimal duration { get; set; }
+        public string language { get; set; }
+        public string platform { get; set; }
+        public string format { get; set; }
+        public string ratio { get; set; }
+        public DateTime EnteredOn { get; set; }
+
+        public string Company { get; set; }
+
+        public string EnteredByUserName
+        {
+            get; set;
+
+        }
+    }
+}
