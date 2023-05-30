b25afd373a529c913a5ad41aeacd21e21869679d
DipteshPatelAidem <dipteshp@aidem.in>
2023-05-30 17:30:54 +0530

    Hdader for save

--- Models/UCNDigitalMasterConfigHeader.cs
+++ Models/UCNDigitalMasterConfigHeader.cs
@@ -6,17 +6,9 @@ using System.Web;
 namespace FullStack.API.Models
 {
     public class UCNDigitalMasterConfigHeader
-    {
-        public int? MasterConfigHeaderID { get; set; }
-        public string MasterConfigHeaderName { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime? EndDate { get; set; }
-        public string Company { get; set; }
-        public string EnteredByUserName { get; set; }
+    {  
         public int CompanyID { get; set; }
         public int EnteredBy { get; set; }
-        public DateTime EnteredOn { get; set; }
-
         public List<UCNDigitalMasterConfig> ucndigitalmasterconfig { get; set; }
 
     }
