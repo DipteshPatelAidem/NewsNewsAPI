42e7a283076a39cf57091b9224d6fd015d87ab11
DipteshPatelAidem <dipteshp@aidem.in>
2023-05-30 17:28:05 +0530

    Startdate EndDate commented

---
+++ Controllers/UCNDigitalMasterConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FullStack.API.Models
+{
+    public class UCNDigitalMasterConfig
+    {
+       // public int? MasterConfigID { get; set; }
+        public string key { get; set; }
+        public string limit { get; set; }
+        public string prefix { get; set; }
+        public int index { get; set; }
+        public int CompanyID {get; set; }
+        public int EnteredBy { get; set; }
+        public int MasterConfigHeaderID { get; set; }
+        //  public DateTime StartDate { get; set; }
+        //  public DateTime? EndDate { get; set; }
+
+        public DateTime EnteredOn { get; set; }
+    }
+}
