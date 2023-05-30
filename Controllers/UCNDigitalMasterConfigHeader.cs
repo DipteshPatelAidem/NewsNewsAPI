c96f191c0430a82a0bf78a19c89bcca7c648afe2
DipteshPatelAidem <dipteshp@aidem.in>
2023-05-30 17:29:56 +0530

    Header model used for History

---
+++ Controllers/UCNDigitalMasterConfigHeader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FullStack.API.Models
+{
+    public class UCNDigitalMasterConfigHeader
+    {  
+        public int CompanyID { get; set; }
+        public int EnteredBy { get; set; }
+        public List<UCNDigitalMasterConfig> ucndigitalmasterconfig { get; set; }
+
+    }
+}
